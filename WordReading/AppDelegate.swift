0b8313f19d3f38f7cafb3cbfd5fdcee04a63ad99
satoshi2110 <zappawater@gmail.com>
2025-03-04 17:42:21 +0900

    アイコンの追加

--- WordReading/AppDelegate.swift
+++ WordReading/AppDelegate.swift
@@ -12,7 +12,9 @@ import RealmSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        
+        
+        
         deleteDataOlderThanOneYear() // 1年前のデータを削除
         return true
     
--- WordReading/StartViewController.swift
+++ WordReading/StartViewController.swift
@@ -12,12 +12,28 @@ class StartViewController: UIViewController {
     @IBOutlet weak var titleLabal: UILabel!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        // UILabelの設定
+        titleLabal.textAlignment = .left // テキストを左揃えに設定
+        titleLabal.numberOfLines = 1
+        titleLabal.lineBreakMode = .byClipping
+        titleLabal.baselineAdjustment = .alignBaselines
+
+        // AutoLayout制約をプログラムで追加
+        titleLabal.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabal.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: -130), // 左端を画面中央から100ポイント左に固定
+            
+            titleLabal.widthAnchor.constraint(equalToConstant: 250) // UILabelの幅を固定
+        ])
+
+        // テキストを一文字ずつ表示
         titleLabal.text = ""
         var charIndex = 0.0
-        let titleText = "ひらがな　を　よもう"
+        let titleText = "よめるかな？"
         for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
-                self.titleLabal.text?.append(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.5 * charIndex, repeats: false) { (timer) in
+                self.titleLabal.text?.append(String(letter))
             }
             charIndex += 1
         }
